cbe6aa668913923e9297f8b7211380c66997b89d
karelyarias2654-afk <karely.arias2654@alumnos.udg.mx>
2026-02-08 14:21:02 -0600

    Refactor Intersection Observer and improve message display
    Updated Intersection Observer settings and added rootMargin.

--- docs/script.js
+++ docs/script.js
@@ -17,7 +17,8 @@ const observer = new IntersectionObserver((entries) => {
     }
   });
 }, {
-  threshold: 0.2
+  threshold: 0.1,
+  rootMargin: "0px 0px -80px 0px"
 });
 
 elementos.forEach(el => observer.observe(el));
@@ -67,3 +68,4 @@ window.addEventListener("DOMContentLoaded", () => {
     audio.play();
   });
 });
+
